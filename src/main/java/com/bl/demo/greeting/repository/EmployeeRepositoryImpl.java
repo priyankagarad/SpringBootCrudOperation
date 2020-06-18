8e341563436d18a09050fe47dec86a729bfa7ecc
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-18 22:01:01 +0530

    ADDED: Declared Method in Interface

--- src/main/java/com/bl/demo/greeting/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/bl/demo/greeting/repository/EmployeeRepositoryImpl.java
@@ -1,4 +1,29 @@
 package com.bl.demo.greeting.repository;
 
-public class EmployeeRepositoryImpl{
+import com.bl.demo.greeting.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeRepositoryImpl implements EmployeeRepository{
+    @Override
+    public List<Employee> get() {
+        return null;
+    }
+
+    @Override
+    public Employee get(int id) {
+        return null;
+    }
+
+    @Override
+    public void save(Employee employee) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
